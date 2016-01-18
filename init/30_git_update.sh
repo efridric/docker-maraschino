58d4a30adbe256fe85992c9a8a89092cf92695af
efridric <efridric@uncc.edu>
2016-01-18 11:14:21 -0500

    Switched to plex maraschino

--- init/30_git_update.sh
+++ init/30_git_update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ ! -d "/app/maraschino/.git" ]; then
-/sbin/setuser abc git clone https://github.com/mrkipling/maraschino.git /app/maraschino
+/sbin/setuser abc git clone https://github.com/gugahoi/maraschino.git /app/maraschino
 else
 cd /app/maraschino
 /sbin/setuser abc git pull
